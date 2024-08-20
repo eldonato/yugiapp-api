22278e61fa94deaf750def7a56b1a53d1a0f5a1d
Leonardo Donato <kqjut3@gmail.com>
2024-08-20 18:14:43 -0300

    criação inicial + adicionando swagger

---
+++ src/main/java/don/leo/yugiapp/YugiappApplication.java
@@ -0,0 +1,13 @@
+package don.leo.yugiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YugiappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YugiappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/don/leo/yugiapp/YugiappApplicationTests.java
@@ -0,0 +1,13 @@
+package don.leo.yugiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YugiappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
