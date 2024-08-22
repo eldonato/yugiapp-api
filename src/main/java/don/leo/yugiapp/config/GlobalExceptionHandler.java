2f3a4662faef1803d8782c52dc88ce34ed64a4b0
Leonardo Donato <kqjut3@gmail.com>
2024-08-22 13:26:24 -0300

    feat: adicionando tratamento EntityNotFoundException, ajustando criação de resposta de erro

--- src/main/java/don/leo/yugiapp/config/GlobalExceptionHandler.java
+++ src/main/java/don/leo/yugiapp/config/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package don.leo.yugiapp.config;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,46 +15,72 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorMessage> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return new ResponseEntity<>(new ErrorMessage(ex), HttpStatus.BAD_REQUEST);
+        var error = new ErrorMessage(ex);
+        return new ResponseEntity<>(error, error.httpStatus());
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<ErrorMessage> handleIllegalArgumentException(DataIntegrityViolationException ex) {
-        var status = HttpStatus.BAD_REQUEST;
-        var errorMessage = new ErrorMessage(extrairMensagemDatabase(ex), status.value());
-        return new ResponseEntity<>(errorMessage, status);
+    public ResponseEntity<ErrorMessage> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        var error = new ErrorMessage(ex);
+        return new ResponseEntity<>(error, error.httpStatus());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handleIEntityNotFoundException(EntityNotFoundException ex) {
+        var error = new ErrorMessage(ex);
+        return new ResponseEntity<>(error, error.httpStatus());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorMessage> handleGenericException(Exception ex) {
-        return new ResponseEntity<>(new ErrorMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    private String extrairMensagemDatabase(DataIntegrityViolationException ex) {
-        Pattern pattern = Pattern.compile("(?s)\\[(.*?)]");
-        Matcher matcher = pattern.matcher(ex.getMessage());
-        String mensagem;
-
-        if (matcher.find()) {
-            mensagem = matcher.group(1);
-        } else {
-            mensagem = "Houve um erro ao acessar os dados da aplicação";
-        }
-
-        return mensagem;
+        var error = new ErrorMessage();
+        return new ResponseEntity<>(error, error.httpStatus());
     }
 
     public record ErrorMessage(
             String erro,
-            int status
+            HttpStatus httpStatus,
+            int statusCode
     ) {
         public ErrorMessage(IllegalArgumentException ex) {
-            this(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+            this(
+                    ex.getMessage(),
+                    HttpStatus.BAD_REQUEST,
+                    HttpStatus.BAD_REQUEST.value());
+        }
+
+        public ErrorMessage(EntityNotFoundException ex) {
+            this(
+                    ex.getMessage(),
+                    HttpStatus.NOT_FOUND,
+                    HttpStatus.NOT_FOUND.value());
+        }
+
+        public ErrorMessage(DataIntegrityViolationException ex) {
+            this(
+                    extrairMensagemDatabase(ex),
+                    HttpStatus.NOT_FOUND,
+                    HttpStatus.NOT_FOUND.value());
         }
 
         public ErrorMessage() {
             this("Erro: Ocorreu um erro interno no servidor.",
+                    HttpStatus.INTERNAL_SERVER_ERROR,
                     HttpStatus.INTERNAL_SERVER_ERROR.value());
+        }
+
+        private static String extrairMensagemDatabase(DataIntegrityViolationException ex) {
+            Pattern pattern = Pattern.compile("(?s)\\[(.*?)]");
+            Matcher matcher = pattern.matcher(ex.getMessage());
+            String mensagem;
+
+            if (matcher.find()) {
+                mensagem = matcher.group(1);
+            } else {
+                mensagem = "Houve um erro ao acessar os dados da aplicação";
+            }
+
+            return mensagem;
         }
     }
 }
